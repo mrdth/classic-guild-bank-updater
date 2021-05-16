9dbb6634502b3e2213942fe4654d5b1e64217ce4
mrdth <mrdth@zeventien.org>
2021-05-16 18:37:24 +0100

    TDD basic config store

---
+++ src/Config.ts
@@ -0,0 +1,28 @@
+interface ConfigItem {
+  key: string,
+  value: any
+}
+
+class Config {
+  private store: ConfigItem[] = [];
+
+  constructor() {
+    this.store.push({ key: 'version', value: 1 });
+  }
+
+  get(key: string) {
+    const item: ConfigItem = this.store.filter((item) => item.key === key)[0] ?? { key: key, value: null };
+    return item.value ?? null;
+  }
+
+  put(item: ConfigItem) {
+    this.delete(item.key);
+    this.store.push(item);
+  }
+
+  delete(key: string) {
+    this.store = this.store.filter((element) => element.key !== key);
+  }
+}
+
+export default new Config();
